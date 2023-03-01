1f20921647059814be380956a2644ca6adc31d37
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-03-01 08:14:15 -0300

    Rotas , controller, inserção usuario concluido

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -16,5 +16,26 @@ class IndexController extends Action{
                 $this->render('inscreverse');
             }
 
+            public function registrar(){
+                exibirArray($_POST);
+
+                foreach ($_POST as $key => $value) {
+                    $$key = $value;
+                }
+
+                $usuario = Container::getModel('Usuario');
+
+                $usuario->__set('nome', $nome);
+                $usuario->__set('email', $email);
+                $usuario->__set('senha', $senha);
+
+                exibirArray($usuario);
+
+                $usuario->salvar();
+                //sucesso
+
+                //erro
+            }
+
         }
 ?>
